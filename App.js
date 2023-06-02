21b072a2494a180ff512902d5d6ad9e068dc44c9
kshitij <63649035+kshitij1127@users.noreply.github.com>
2023-06-02 16:19:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Camera from './screens/Camera';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Camera />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
